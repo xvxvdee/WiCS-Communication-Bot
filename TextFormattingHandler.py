b11b3ba613dbb4254404e96cc245674ff8e2e1f7
xvxvdee <deandra.spikemadden@gmail.com>
2024-01-10 18:40:49 -0500

    Create method to turn readme table into a dataframe

--- TextFormattingHandler.py
+++ TextFormattingHandler.py
@@ -1,7 +1,25 @@
+import re
+import pandas as pd
+
 class TextFormattingHandler:
 
-    def __init__(self):
-        pass
+    def readme_to_dataframe(self, text):
+        # Locate readme table
+        result = text.split("Please leave a one line gap between this and the table TABLE_START (DO NOT CHANGE THIS LINE) -->\\n\\n")
+        table_top = result[1]
+        markdown_table = table_top.split("\\n\\n<!-- Please leave a one line gap between this and the table TABLE_END (DO NOT CHANGE THIS LINE) -->")    
+        markdown_table_rows = markdown_table[0].split("\\n")[2:] 
+        table = [re.split('\|', x) for x in markdown_table_rows]
+
+        # Create dataframe
+        df = pd.DataFrame(table,columns=["EmptyFront","Company","Role","Location","Application/Link","Date Posted","EmptyBack"])
+        df.set_index('Company', inplace=True)
+
+        # Drop empty columns
+        df = df.drop("EmptyFront",axis=1)
+        df = df.drop("EmptyBack",axis=1)
+        return df
+
 
     # Format text for Embeddings # job posting channel
     def internship_embed_formatter(self):
