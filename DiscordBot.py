ce1dae545fea831e0251af5a91a7d1fdb5e62d27
xvxvdee <deandra.spikemadden@gmail.com>
2024-01-25 16:00:52 -0500

    Took data from dataframes and created embeds. Set message to send every 24hrs

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,24 +1,94 @@
-import discord #import discord library
-import os #used for the token variable, requires that we use .env file
-import requests
-import base64
-import json
-from JobScrapingService import JobScrapingService #used to import the job scraping class from the job scrape py
-# create a .env file with the token name; find a way to make a private doc on github since the bot will not be run locally
+import discord 
+from discord.ext import tasks,commands
+import os
+from dotenv import load_dotenv 
+from JobPostingFetcher import JobPostingFetcher 
+from DataFrameAccessor import DataFrameAccessor
 
-posting = JobScrapingService
-client = discord.Client() #connection to discord
+# Setup Bot
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+client = commands.Bot(command_prefix="$",intents=discord.Intents.default()) 
 
-@client.event #register event: ready for use
+# Create instances for postings
+job_fetcher = JobPostingFetcher()
+data_accessor = DataFrameAccessor()
+newgrad_icon = "https://cdn-icons-png.flaticon.com/128/1991/1991047.png"
+internship_icon = "https://cdn-icons-png.flaticon.com/128/7376/7376577.png"
+offseason_icon = "https://cdn-icons-png.flaticon.com/128/2117/2117739.png"
+
+@client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
+    
+    send_new_grad_roles.start()
+    send_summer_roles.start()
+    send_offseason_roles.start()
 
-@client.event #register event: recieve message and execute
-async def on_message(message):
-    if message.author == client.user:
+@tasks.loop(hours=24)
+async def send_new_grad_roles():
+    job_channel = client.get_channel(1200151138645856266)
+    #plugs = client.get_channel(817211947908595713)
+
+    df_posting = job_fetcher.latest_newgrad_postings()
+    newgrad_color = 0xf2f0ff
+
+    try:
+        for i, row in df_posting.iterrows():
+            embed=discord.Embed(title="NEW GRAD POSTING", description=data_accessor.get_company_text(row), color=newgrad_color)
+            embed.set_thumbnail(url=newgrad_icon)
+            embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
+            embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
+            embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
+            embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+            embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
+            await job_channel.send(embed=embed)
+           # await plugs.send(embed=embed)
+    except(AttributeError) as err:
+        print(err)
         return
 
-    if message.content.startswith('$hello'): #needs to be some sort of trigger; not sure what it should be or how to enable it
-        await message.channel.send(posting) #posting needs to be formattd; not sure how to do that...
 
-client.run(os.getenv('TOKEN')) #runs the bot with the login token from the .env file
+@tasks.loop(hours=24)
+async def send_summer_roles():
+    job_channel = client.get_channel(1200151138645856266)
+    df_posting = job_fetcher.latest_internship_postings()
+    newgrad_color = 0xd1c171
+    try:
+        for i, row in df_posting.iterrows():
+            embed=discord.Embed(title="SUMMER INTERNSHIP POSTING", description=data_accessor.get_company_text(row), color=newgrad_color)
+            embed.set_thumbnail(url=internship_icon)
+            embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
+            embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
+            embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
+            embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+            embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
+            await job_channel.send(embed=embed)
+    except(AttributeError) as err:
+        print(err)
+        return
+
+@tasks.loop(hours=24)
+async def send_offseason_roles():
+    job_channel = client.get_channel(1200151138645856266)
+    df_posting = job_fetcher.latest_offseason_postings()
+    newgrad_color = 0x71b4d1
+
+    try:
+        for i, row in df_posting.iterrows():
+            embed=discord.Embed(title="OFFSEASON INTERNSHIP POSTING", description=data_accessor.get_company_text(row), color=newgrad_color)
+            embed.set_thumbnail(url=internship_icon)
+            embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
+            embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
+            embed.add_field(name="Term(s)", value=data_accessor.get_terms(row), inline=False)
+            embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
+            embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+            embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
+            await job_channel.send(embed=embed)
+    except(AttributeError) as err:
+        print(err)
+        return
+
+client.run(TOKEN) #runs the bot with the login token from the .env file
+
+
