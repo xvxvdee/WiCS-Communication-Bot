d883f1691655089eadde243087e56f5d8ec882f6
xvxvdee <deandra.spikemadden@gmail.com>
2024-02-23 15:57:50 -0500

    Modified to work with LoggingService.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,15 +5,23 @@ import os
 from dotenv import load_dotenv 
 from JobPostingFetcher import JobPostingFetcher 
 from DataFrameAccessor import DataFrameAccessor
+from LoggingService import LoggingService
+from TextFormattingHandler import TextFormattingHandler
+from JobScrapingService import JobScrapingService
 
 # Setup Bot
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+URI = os.getenv("MONGODB_URI")
 client = commands.Bot(command_prefix="$",intents=discord.Intents.default()) 
 
 # Create instances for postings
-job_fetcher = JobPostingFetcher()
-data_accessor = DataFrameAccessor()
+logger = LoggingService(URI)
+data_accessor = DataFrameAccessor(logger)
+formatter = TextFormattingHandler()
+data_scraper = JobScrapingService(logger)
+job_fetcher = JobPostingFetcher(formatter,data_scraper,data_accessor)
+
 newgrad_icon = "https://cdn-icons-png.flaticon.com/128/1991/1991047.png"
 internship_icon = "https://cdn-icons-png.flaticon.com/128/7376/7376577.png"
 offseason_icon = "https://cdn-icons-png.flaticon.com/128/2117/2117739.png"
@@ -21,130 +29,162 @@ offseason_icon = "https://cdn-icons-png.flaticon.com/128/2117/2117739.png"
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    
+    logger.log_startup()
     try:
         send_new_grad_roles.start()
     except Exception as ex:
-        print(f"EXCEPTION TASK START:\n>New grad roles\n>{ex}")
+        logger.log_task_start_exception("TASK START EXCEPTION: New grad roles",ex)
     try:
         send_summer_roles.start()
     except Exception as ex:
-        print(f"EXCEPTION TASK START:\n>Summer roles\n>{ex}")
+        logger.log_task_start_exception("TASK START EXCEPTION: Summer roles",ex)
     try:
         send_offseason_roles.start()
     except Exception as ex:
-        print(f"EXCEPTION TASK START:\n>Offseason roles\n>{ex}")
-
-
+        logger.log_task_start_exception("TASK START EXCEPTION: Offseason roles",ex)
 
 @tasks.loop(hours=24)
 async def send_new_grad_roles():
-    job_channel = client.get_channel(1202309603602464768)
-    plugs = client.get_channel(817211947908595713)
+    logger.check_space()
+    job_channel = client.get_channel(1200151138645856266)
+    #plugs = client.get_channel(817211947908595713)
     df_posting = job_fetcher.latest_newgrad_postings()
     newgrad_color = 0xf2f0ff
 
     try:
         for i, row in df_posting.iterrows():
-            print(f"Posting New Grad role ... {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)}")
-            if data_accessor.get_posted(row): 
-                print(f"> {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)} was already posted today.\n")
+            data = row
+            hash_id=data_accessor.create_id("newgrad", data)
+            doc_exist = logger.update_postings_db(data,hash_id)
+
+            if doc_exist is not None: data = doc_exist
+
+            company = data_accessor.get_company_text(data)
+            role = data_accessor.get_role(data)
+            date_posted =data_accessor.get_date_posted(data)
+            locations = data_accessor.get_locations(data)
+            link = data_accessor.get_application_link(data)
+
+            logger.log_posting(data,f"Posting New Grad role for {company}: {role}")
+
+            if logger.check_shared_status(hash_id):
+                logger.log_catch_duplicate(data,f"> {company}: {role} was already posted today.\n")
                 continue
             else:
-                embed=discord.Embed(description="New Grad", title=data_accessor.get_company_text(row), color=newgrad_color)
+                embed=discord.Embed(description="New Grad", title=company, color=newgrad_color)
                 embed.set_thumbnail(url=newgrad_icon)
-                embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
-                embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
-                embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
-                embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+                embed.add_field(name="Role", value=role, inline=True)
+                embed.add_field(name="Date Posted", value=date_posted, inline=True)
+                embed.add_field(name="Location", value=locations, inline=False)
+                embed.add_field(name="Application Link", value=link, inline=False)
                 embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
                 
                 try:
                     await job_channel.send(embed=embed)
-                    await plugs.send(embed=embed)
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/newgrad_postings.csv",header=True, index=True)
-                except Exception as e:
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/newgrad_postings.csv",header=True, index=True)
-                    print(f"New grad role {data_accessor.get_company_text(row)} failed to send.\n{str(e)}")
-                time.sleep(5);
+                    # await plugs.send(embed=embed)
+                    logger.set_sucessful_shared_status(hash_id)
+                except Exception as ex:
+                    logger.set_failed_shared_status(hash_id)
+                    logger.log_post_failure(data,f"New grad role {company} failed to send",str(ex))
+                time.sleep(5)
 
     except(AttributeError) as err:
-        print(f"TASK ERROR: While sending New Grad roles...\n{err}")
+        logger.log_task_exception("TASK ERROR: While sending New Grad roles",err)
         return
-
 
 @tasks.loop(hours=24)
 async def send_summer_roles():
-    job_channel = client.get_channel(1202309603602464768)
+    logger.check_space()
+    job_channel = client.get_channel(1200151138645856266)
     df_posting = job_fetcher.latest_internship_postings()
     summer_color = 0xd1c171
     try:
         for i, row in df_posting.iterrows():
-            print(f"Posting internship ... {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)}")
-            if data_accessor.get_posted(row): 
-                print(f"> {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)} was already posted today.\n")
+            
+            data = row
+            hash_id=data_accessor.create_id("internship",data)
+            doc_exist = logger.update_postings_db(data,hash_id)
+
+            if doc_exist is not None: data = doc_exist
+
+            company = data_accessor.get_company_text(data)
+            role = data_accessor.get_role(data)
+            date_posted =data_accessor.get_date_posted(data)
+            locations = data_accessor.get_locations(data)
+            link = data_accessor.get_application_link(data)
+
+            logger.log_posting(data,f"Posting Summer role for {company}: {role}")
+
+            if logger.check_shared_status(hash_id): 
+                logger.log_catch_duplicate(data,f"> {company}: {role} was already posted today.\n")
                 continue
             else:
-                embed=discord.Embed(description="Summer Internship", title=data_accessor.get_company_text(row), color=summer_color)
+                embed=discord.Embed(description="Summer Internship", title=company, color=summer_color)
                 embed.set_thumbnail(url=internship_icon)
-                embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
-                embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
-                embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
-                embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+                embed.add_field(name="Role", value=role, inline=True)
+                embed.add_field(name="Date Posted", value=date_posted, inline=True)
+                embed.add_field(name="Location", value=locations, inline=False)
+                embed.add_field(name="Application Link", value=link, inline=False)
                 embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
                 try:
                     await job_channel.send(embed=embed)
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/internship_postings.csv",header=True, index=True)
+                    logger.set_sucessful_shared_status(hash_id)
 
-                except Exception as e:
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/newgrad_postings.csv",header=True, index=True)
-                    print(f"Internship role {data_accessor.get_company_text(row)} failed to send.\n{str(e)}")
-                time.sleep(5);
+                except Exception as ex:
+                    logger.set_failed_shared_status(hash_id)
+                    logger.log_post_failure(data,f"Summer role {company} failed to send",str(ex))
+                time.sleep(5)
     except(AttributeError) as err:
-        print(f"TASK ERROR: While sending Internship roles...\n{err}")
+        logger.log_task_exception("TASK ERROR: While sending summer roles",err)
         return
 
 @tasks.loop(hours=24)
 async def send_offseason_roles():
-    job_channel = client.get_channel(1202309603602464768)
+    logger.check_space()
+    job_channel = client.get_channel(1200151138645856266)
     df_posting = job_fetcher.latest_offseason_postings()
     offseason_color = 0x71b4d1
 
     try:
         for i, row in df_posting.iterrows():
-            print(f"Posting offseason internship ... {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)}")
-            if data_accessor.get_posted(row): 
-                print(f"> {data_accessor.get_company_text(row)}: {data_accessor.get_role(row)} was already posted today.\n")
+            data = row
+            hash_id=data_accessor.create_id("offseason",data)
+            doc_exist = logger.update_postings_db(data,hash_id)
+
+            if doc_exist is not None: data = doc_exist
+
+            company = data_accessor.get_company_text(data)
+            role = data_accessor.get_role(data)
+            date_posted =data_accessor.get_date_posted(data)
+            locations = data_accessor.get_locations(data)
+            link = data_accessor.get_application_link(data)
+            
+            logger.log_posting(data,f"Posting Offseason role for {company}: {role}")
+
+            if logger.check_shared_status(hash_id): 
+                logger.log_catch_duplicate(data,f"> {company}: {role} was already posted today.\n")
                 continue
             else:
-                embed=discord.Embed(description="Offseason Internship", title=data_accessor.get_company_text(row), color=offseason_color)
+                embed=discord.Embed(description="Offseason Internship", title=company, color=offseason_color)
                 embed.set_thumbnail(url=offseason_icon)
-                embed.add_field(name="Role", value=data_accessor.get_role(row), inline=True)
-                embed.add_field(name="Date Posted", value=data_accessor.get_date_posted(row), inline=True)
+                embed.add_field(name="Role", value=role, inline=True)
+                embed.add_field(name="Date Posted", value=date_posted, inline=True)
                 embed.add_field(name="Term(s)", value=data_accessor.get_terms(row), inline=False)
-                embed.add_field(name="Location", value=data_accessor.get_locations(row), inline=False)
-                embed.add_field(name="Application Link", value=data_accessor.get_application_link(row), inline=False)
+                embed.add_field(name="Location", value=locations, inline=False)
+                embed.add_field(name="Application Link", value=link, inline=False)
                 embed.set_footer(text="Resume icons created by juicy_fish - Flaticon")
                 
                 try:
                     await job_channel.send(embed=embed)
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/offseason_postings.csv",header=True, index=True)
+                    logger.set_sucessful_shared_status(hash_id)
 
-                except Exception as e:
-                    data_accessor.update_posted_status(i,df_posting)
-                    df_posting.to_csv("Data/newgrad_postings.csv",header=True, index=True)
-                    print(f"Offseason role {data_accessor.get_company_text(row)} failed to send.\n{str(e)}")
+                except Exception as ex:
+                    logger.set_failed_shared_status(hash_id)
+                    logger.log_post_failure(data,f"Offseason role {company} failed to send",str(ex))
                 time.sleep(5)
 
     except(AttributeError) as err:
-        print(f"TASK ERROR: While sending Offseason internship roles...\n{err}")
+        logger.log_task_exception("TASK ERROR: While sending Offseason roles",err)
         return
 
 client.run(TOKEN) #runs the bot with the login token from the .env file
-
-
